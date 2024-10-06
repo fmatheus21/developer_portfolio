4ddaba83cdcea9ae44711b8f233328440abbdffe
Fernando Matheus <fernando.matheuss@hotmail.com>
2024-10-06 18:35:37 -0300

    Implementando html

--- src/app/pages/components/section-service/section-service.component.ts
+++ src/app/pages/components/section-service/section-service.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../../core/service/app.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-section-service',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './section-service.component.html',
-  styleUrl: './section-service.component.css'
+  styleUrl: './section-service.component.css',
 })
-export class SectionServiceComponent {
+export class SectionServiceComponent implements OnInit {
+  services: any[] = [];
+  selectedService: any;
 
+  constructor(private appService: AppService) {}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load() {
+    this.appService.findAll().subscribe((data: any) => {
+      this.services = data['services'];
+    });
+  }
+
+  openModal(service: any) {
+    this.selectedService = service;
+  }
 }
