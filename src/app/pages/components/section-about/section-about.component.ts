c5fa7db239d51c31e7ee1e8da9dcdb569f7f3d3d
Fernando Matheus <fernando.matheuss@hotmail.com>
2024-10-07 04:20:05 -0300

    Implementando html

--- src/app/pages/components/section-about/section-about.component.ts
+++ src/app/pages/components/section-about/section-about.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../../core/service/app.service';
 
 @Component({
   selector: 'app-section-about',
   standalone: true,
   imports: [],
   templateUrl: './section-about.component.html',
-  styleUrl: './section-about.component.css'
+  styleUrl: './section-about.component.css',
 })
-export class SectionAboutComponent {
+export class SectionAboutComponent implements OnInit {
+  about: any;
+  age: any;
 
+  constructor(private appService: AppService) {}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load() {
+    this.appService.findAll().subscribe((data: any) => {
+      this.about = data['about'];
+      var year = new Date().getFullYear();
+      this.age = year - this.about.year + ' anos';
+    });
+  }
 }
